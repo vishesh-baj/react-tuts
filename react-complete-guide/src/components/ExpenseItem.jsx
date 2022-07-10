e82da8fd77097abb56c07d386193c2b62ffc75bb
vishesh_baj <vishesh.bajpayee@icloud.com>
2022-07-10 20:30:54 +0530

    added expense date component

--- react-complete-guide/src/components/ExpenseItem.jsx
+++ react-complete-guide/src/components/ExpenseItem.jsx
@@ -1,14 +1,14 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div>July 10th 2022</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$288.16</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
 };
-
 export default ExpenseItem;
