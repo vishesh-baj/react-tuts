f8e4015264b96f76fff5e2c758a8857359f3b73f
vishesh_baj <vishesh.bajpayee@icloud.com>
2022-07-16 11:08:15 +0530

    added filter by year

--- react-complete-guide/src/components/Expenses/ExpenseDate.jsx
+++ react-complete-guide/src/components/Expenses/ExpenseDate.jsx
@@ -1,4 +1,7 @@
+import React from "react";
+
 import "./ExpenseDate.css";
+
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
@@ -12,4 +15,5 @@ const ExpenseDate = (props) => {
     </div>
   );
 };
+
 export default ExpenseDate;
--- react-complete-guide/src/components/Expenses/Expenses.jsx
+++ react-complete-guide/src/components/Expenses/Expenses.jsx
@@ -1,9 +1,20 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import { useState } from "react";
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2019");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       {props.items.map((expense) => (
         <ExpenseItem
           title={expense.title}
